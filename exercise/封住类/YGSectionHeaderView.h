1af74515d8778fcea2d8fe798ec21ce55f2d2fe8
xu <xusizhe@126.com>
2016-07-08 02:06:05 +0800

    e
    ad

--- exercise/封住类/YGSectionHeaderView.h
+++ exercise/封住类/YGSectionHeaderView.h
@@ -2,7 +2,7 @@
 //  YGSectionHeaderView.h
 //  MobileUU
 //
-//  Created by UTOUU on 16/3/19.
+//  Created by 哲 on 16/3/19.
 //  Copyright © 2016年 Shanghai Pecker Network Technology. All rights reserved.
 //
 
